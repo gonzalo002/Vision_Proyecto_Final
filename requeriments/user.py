2a087425eb35ef18dc3ec5e9ddbec0634b025fc8
Itsasne <ipresumido@opendeusto.es>
2024-12-16 21:43:13 +0100

    correct proyect

---
+++ requeriments/user.py
@@ -0,0 +1,23 @@
+USER_THEMES = {
+    "custom-vision": {
+        "type": "light",
+        "colors": {
+            "primary": "#5eaae8",
+            "secondary": "#4b89bc",
+            "success": "#aad68d",
+            "info": "#a6daf3",
+            "warning": "#dbb940",
+            "danger": "#db5940",
+            "light": "#a6daf3",
+            "dark": "#5a5a5a",
+            "bg": "#ffffff",
+            "fg": "#5a5a5a",
+            "selectbg": "#6140dc",
+            "selectfg": "#ffffff",
+            "border": "#ced4da",
+            "inputfg": "#696969",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    },
+}
