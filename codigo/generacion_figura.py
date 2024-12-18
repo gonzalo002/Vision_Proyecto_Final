a4028af2b8aaf1feec09065f211cf3c38ee8bf41
Gonzalo_Maldonado <96051791+gonzalo002@users.noreply.github.com>
2024-12-18 19:07:48 +0100

    Update generacion_figura.py

--- codigo/generacion_figura.py
+++ codigo/generacion_figura.py
@@ -1,5 +1,3 @@
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 from copy import deepcopy
@@ -10,11 +8,11 @@ class FigureGenerator:
         self.message = None
         self.message_type = 0 # 1=Info, 2=Warn, 3=Error
 
-    def generate_figure_from_matrix(self, plant_matrix, front_matrix, side_matrix, figsize:tuple=(3,3), paint:bool = False, tkinter:bool=False):
+    def generate_figure_from_matrix(self, matriz_planta:list, matriz_alzado:list, matriz_perfil:list, figsize:tuple=(3,3), paint:bool = False, tkinter:bool=False):
 
-        anchura, profundidad, plant_matrix_recortada = self._cut_matrix_finding_shape(plant_matrix)
-        altura, anchura2, front_matrix_recortada = self._cut_matrix_finding_shape(front_matrix)
-        altura2, profundidad2, side_matrix_recortada = self._cut_matrix_finding_shape(side_matrix)
+        anchura, profundidad, matriz_planta_recortada = self._cut_matrix_finding_shape(matriz_planta)
+        altura, anchura2, matriz_alzado_recortada = self._cut_matrix_finding_shape(matriz_alzado)
+        altura2, profundidad2, matriz_perfil_recortada = self._cut_matrix_finding_shape(matriz_perfil)
         
         if (altura is None) and (anchura is None) and (profundidad is None):
             return self._paint_matrix(np.array([[[]]]), figsize, tkinter)
@@ -31,51 +29,53 @@ class FigureGenerator:
             self.message_type = 3
             return self._paint_matrix(np.array([[[]]]), figsize, tkinter)
 
-        self.matriz3D = deepcopy(np.full((anchura, altura, profundidad), -1))
-        matriz3D = deepcopy(np.full((anchura, altura, profundidad), -1))
+        self.matriz3D = deepcopy(np.full((profundidad, altura, anchura), -1))
+        matriz3D = deepcopy(np.full((profundidad, altura, anchura), -1))
+
+        # Definir el tamaño de cada cubo
+        size = 1
 
         for columna_planta in range(profundidad-1, -1, -1):
             for fila_planta in range(anchura):
-                if plant_matrix_recortada[fila_planta][columna_planta] != -1:
-                    color_cubo = plant_matrix_recortada[fila_planta][columna_planta]
-                    columna_lateral = x = profundidad - columna_planta - 1
-                    columna_frontal = anchura - fila_planta -1
+                if matriz_planta_recortada[fila_planta][columna_planta] != -1:
+                    color_cubo = matriz_planta_recortada[fila_planta][columna_planta]
+                    columna_perfil = x = profundidad - columna_planta - 1
+                    columna_alzado =  anchura - fila_planta -1
                     y = fila_planta
                     cube_found = False
-                    for fila_lateral in range(altura):
-                        fila_frontal = fila_lateral
-                        z = altura - fila_lateral - 1
-                        if not cube_found and plant_matrix_recortada[fila_planta][columna_planta] == side_matrix_recortada[fila_lateral][columna_lateral] and plant_matrix_recortada[fila_planta][columna_planta] == front_matrix_recortada[fila_frontal][columna_frontal]:
+                    for fila_alzado in range(altura):
+                        fila_perfil = fila_alzado
+                        z = altura - fila_alzado - 1
+                        if not cube_found and matriz_planta_recortada[fila_planta][columna_planta] == matriz_perfil_recortada[fila_perfil][columna_perfil] and matriz_planta_recortada[fila_planta][columna_planta] == matriz_alzado_recortada[fila_alzado][columna_alzado]:
                             matriz3D[x][z][y] = color_cubo
-                            side_matrix_recortada[fila_lateral][columna_lateral] = front_matrix_recortada[fila_frontal][columna_frontal] = 5
+                            matriz_perfil_recortada[fila_perfil][columna_perfil] = matriz_alzado_recortada[fila_alzado][columna_alzado] = 5
                             cube_found = True
 
-                        elif not cube_found and plant_matrix_recortada[fila_planta][columna_planta] == front_matrix_recortada[fila_frontal][columna_frontal] and side_matrix_recortada[fila_lateral][columna_lateral] != -1:
+                        elif not cube_found and matriz_planta_recortada[fila_planta][columna_planta] == matriz_alzado_recortada[fila_alzado][columna_alzado] and matriz_perfil_recortada[fila_perfil][columna_perfil] != -1:
                             matriz3D[x][z][y] = color_cubo
-                            front_matrix_recortada[fila_frontal][columna_frontal] = 5
+                            matriz_alzado_recortada[fila_alzado][columna_alzado] = 5
                             cube_found = True
 
-                        elif not cube_found and plant_matrix_recortada[fila_planta][columna_planta] == side_matrix_recortada[fila_lateral][columna_lateral] and front_matrix_recortada[fila_frontal][columna_frontal] != -1:
+                        elif not cube_found and matriz_planta_recortada[fila_planta][columna_planta] == matriz_perfil_recortada[fila_perfil][columna_perfil] and matriz_alzado_recortada[fila_alzado][columna_alzado] != -1:
                             if columna_planta == profundidad-1:
                                 pass
                             else:
                                 matriz3D[x][z][y] = color_cubo
-                                side_matrix_recortada[fila_lateral][columna_lateral] = 5
+                                matriz_perfil_recortada[fila_perfil][columna_perfil] = 5
                                 cube_found = True
 
-                        elif cube_found and side_matrix_recortada[fila_lateral][columna_lateral] == front_matrix_recortada[fila_frontal][columna_frontal] and front_matrix_recortada[fila_frontal][columna_frontal] != 5:
-                            matriz3D[x][z][y] = side_matrix_recortada[fila_lateral][columna_lateral]
-                            side_matrix_recortada[fila_lateral][columna_lateral] = front_matrix_recortada[fila_frontal][columna_frontal] = 5
+                        elif cube_found and matriz_perfil_recortada[fila_perfil][columna_perfil] == matriz_alzado_recortada[fila_alzado][columna_alzado] and matriz_alzado_recortada[fila_alzado][columna_alzado] != 5:
+                            matriz3D[x][z][y] = matriz_perfil_recortada[fila_perfil][columna_perfil]
+                            matriz_perfil_recortada[fila_perfil][columna_perfil] = matriz_alzado_recortada[fila_alzado][columna_alzado] = 5
+                        elif cube_found and matriz_perfil_recortada[fila_perfil][columna_perfil] != 5 and matriz_alzado_recortada[fila_alzado][columna_alzado] == 5:
+                            matriz3D[x][z][y] = matriz_perfil_recortada[fila_perfil][columna_perfil]
+                            matriz_perfil_recortada[fila_perfil][columna_perfil] = 5
 
-                        elif cube_found and side_matrix_recortada[fila_lateral][columna_lateral] != 5 and front_matrix_recortada[fila_frontal][columna_frontal] == 5:
-                            matriz3D[x][z][y] = side_matrix_recortada[fila_lateral][columna_lateral]
-                            side_matrix_recortada[fila_lateral][columna_lateral] = 5
+                        elif cube_found and matriz_alzado_recortada[fila_alzado][columna_alzado] != 5 and matriz_perfil_recortada[fila_perfil][columna_perfil] == 5:
+                            matriz3D[x][z][y] = matriz_alzado_recortada[fila_alzado][columna_alzado] 
+                            matriz_alzado_recortada[fila_alzado][columna_alzado]  = 5
 
-                        elif cube_found and front_matrix_recortada[fila_frontal][columna_frontal] != 5 and side_matrix_recortada[fila_lateral][columna_lateral] == 5:
-                            matriz3D[x][z][y] = front_matrix_recortada[fila_frontal][columna_frontal] 
-                            front_matrix_recortada[fila_frontal][columna_frontal]  = 5
-
-                        elif cube_found and front_matrix_recortada[fila_frontal][columna_frontal] == 5 and side_matrix_recortada[fila_lateral][columna_lateral] == 5:
+                        elif cube_found and matriz_alzado_recortada[fila_alzado][columna_alzado] == 5 and matriz_perfil_recortada[fila_perfil][columna_perfil] == 5:
                             matriz3D[x][z][y] = 4
 
                     if not cube_found:
@@ -152,228 +152,3 @@ class FigureGenerator:
         else:
             plt.show()
             return None
-    
-
-if __name__ in "__main__":
-    generator = FigureGenerator()
-
-    # Figura 1
-    top_side_1 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 3, 1, -1, -1],
-                    [2, 0, 3, -1, -1]]
-
-    front_side_1 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [0, -1, -1, -1, -1],
-                    [3, 1, -1, -1, -1]]
-
-    # Figura 2
-    top_side_2 = [[-1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1],
-                [0, -1, -1, -1, -1],
-                [2, -1, -1, -1, -1],
-                [0, 3, 1, 3, -1]]
-
-    front_side_2 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [0, -1, -1, -1, -1],
-                    [3, 2, 0, -1, -1]]
-
-    # Figura 3
-    top_side_3 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 3, 0, -1, -1],
-                    [0, 1, 2, -1, -1],
-                    [-1, 3, 1, -1, -1]]
-
-    front_side_3 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [1, 2, 0, -1, -1]]
-
-    # Figura 4
-    top_side_4 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 3, 1, -1, -1],
-                    [3, -1, 2, -1, -1],
-                    [-1, 3, 1, -1, -1]]
-
-    front_side_4 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 3, 1, -1, -1],
-                    [1, 2, 0, -1, -1]]
-
-    # Figura 5
-    top_side_5 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 1, -1, -1, -1],
-                    [2, 3, 1, -1, -1],
-                    [-1, 0, -1, -1, -1]]
-
-    front_side_5 = [[-1, 3, -1, -1, -1],
-                    [-1, 0, -1, -1, -1],
-                    [-1, 3, -1, -1, -1],
-                    [-1, 1, -1, -1, -1],
-                    [0, 1, 1, -1, -1]]
-
-    # Figura 6 y Ultima
-    top_side_6 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [0, 1, 3, -1, -1],
-                    [2, 3, 1, -1, -1],
-                    [1, 2, 3, -1, -1]]
-
-    front_side_6 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [2, 3, 1, -1, -1],
-                    [3, 1, 3, -1, -1]]
-    
-    # Figura 7 y Ultima
-    top_side_7 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [3, 3, 1, -1, -1],
-                    [3, 1, 3, -1, -1],
-                    [3, 3, 3, -1, -1]]
-
-    front_side_7 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 1, -1, -1, -1],
-                    [1, 3, 3, -1, -1]]
-    
-    top_side_8 = [[-1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1],
-                [ 0,  2,  2, -1, -1],
-                [ 3, -1,  1, -1, -1],
-                [ 3,  1,  0, -1, -1]]
-    
-    front_side_8 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1,  2, -1, -1],
-                    [ 3, -1,  1, -1, -1],
-                    [ 0,  2,  3, -1, -1]]
-    
-    front_side_9 = [[-1, -1, -1, -1, -1],
-                    [-1,  0, -1, -1, -1],
-                    [-1,  2,  1, -1, -1],
-                    [-1,  3,  2, -1, -1],
-                    [ 1,  2,  1,  0, -1]]
-    top_side_9 = [[-1, -1, -1, -1, -1],
-                    [ 2,  3,  3,  0, -1],
-                    [ 3,  0,  1,  1, -1],
-                    [ 1,  0,  3,  2, -1],
-                    [ 2,  2,  3,  1, -1]]
-    
-    front_side_10 = [[-1, -1, -1, -1, -1,],
-                    [-1,  -1, -1, -1, -1,],
-                    [ 1,  0, -1, -1, -1,],
-                    [ 0,  2, -1, -1, -1,],
-                    [ 2,  1,  0, 3, -1,]]
-    
-    top_side_10 = [[-1, -1, -1, -1, -1,],
-                [ 3,  2,  1,  3, -1,],
-                [ 1,  3,  3,  0, -1,],
-                [ 1,  3,  0,  1, -1,],
-                [ 2,  1,  0,  2, -1,]]
-    
-    side_matrix_10 = [[-1, -1, -1, -1, -1],
-                    [ -1,  -1,  -1,  -1, -1],
-                    [ -1,  0,  1,  -1, -1],
-                    [ -1,  2,  3,  -1, -1],
-                    [ 3,  1,  2,  3, -1]]
-    
-    front_side_11 = [[-1, -1, -1, -1, -1],
-                     [-1, -1, -1, -1, -1],
-                     [ 0, -1, -1, -1, -1],
-                     [ 1, -1,  0, -1, -1],
-                     [ 1,  1,  2, -1, -1]]
-    top_side_11 = [[-1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1],
-                [ 3,  2,  0, -1, -1],
-                [ 2, -1,  1, -1, -1],
-                [ 0,  3,  1, -1, -1]]
-    
-    front_side_12 = [[-1, -1, -1, -1, -1],
-                     [-1, -1, -1, -1, -1],
-                     [ 1, 0, -1, -1, -1],
-                     [ 0, 2,  -1, -1, -1],
-                     [ 2,  1,  0, 3, -1]]
-    top_side_12 = [[-1, -1, -1, -1, -1],
-                    [3, 2,  1, 3, -1],
-                    [1, 3,  3, 0, -1],
-                    [1, 3,  0, 1, -1],
-                    [2, 1,  0, 2, -1]]
-
-    top_side_13 = [[0, 1, 2, 2, 0],
-                    [-1, -1,  -1, -1, 3],
-                    [-1, -1,  -1, -1, 1],
-                    [-1, -1,  -1, -1, 2],
-                    [-1, -1,  -1, -1, 0]]
-
-    front_side_13 = [[-1, -1, -1, -1, -1],
-                    [-1, -1,  -1, -1, -1],
-                    [-1, -1,  -1, -1, -1],
-                    [0, -1,  1, -1, 0],
-                    [1, 2,  3, 3, 1]]
-    
-    side_matrix_13 = [[-1, -1, -1, -1, -1],
-                    [-1, -1,  -1, -1, -1],
-                    [-1, -1,  -1, -1, -1],
-                    [0, -1,  2, -1, 0],
-                    [1, 2,  1, 1, 3]]
-    
-    front_side_14 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1,],
-                    [-1, -1, -1, -1, -1,],
-                    [-1,  2,  0, -1, -1,],
-                    [ 3,  1,  2,  3, -1,]]
-    top_side_14 = [[-1, -1, -1, -1, -1],
-                [ 3,  3, -1, -1, -1],
-                [ 2,  0,  2, -1, -1],
-                [ 2,  1,  2, -1, -1],
-                [ 3,  3, -1, -1, -1]]
-    side_matrix_14 = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [ 2,  0,  2, -1, -1],
-                    [ 2,  3,  3, -1, -1]]
-    
-    pruebas_front = [[-1, -1, -1, -1, -1],
-                    [-1, -1, -1, -1, -1],
-                    [-1, 0, -1, -1, -1],
-                    [1, 2, -1, -1, -1],
-                    [ 3, 3, -1, -1, -1]]
-
-
-    pruebas_top=[[-1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1],
-                [-1, 0, 1, -1, -1],
-                [3, 1, -1, -1, -1]]
-
-    pruebas_limites_top = [[-1, -1, -1, -1, -1],
-                            [-1, -1, 0, -1, -1],
-                            [-1, 2, -1, -1, -1],
-                            [-1, -1, -1, -1, -1],
-                            [-1, -1, -1, -1, -1]]
-
-    pruebas_limites_front = [[-1, -1, -1, -1, -1],
-                            [-1, -1, -1, -1, -1],
-                            [-1, 0, -1, -1, -1],
-                            [1, 2, -1, -1, -1],
-                            [3, 2, -1, -1, -1]]
-
-    vacia = [[-1, -1, -1, -1, -1],
-            [-1, -1, -1, -1, -1],
-            [-1, -1, -1, -1, -1],
-            [-1, -1, -1, -1, -1],
-            [-1, -1, -1, -1, -1]]
-
-    generator.generate_figure_from_matrix(top_side_14, front_side_14, side_matrix_14, paint=True)
